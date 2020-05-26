42b5415a4741ef752c276924eb07d28d69c1d858
mitchellhamilton <mitchell@hamil.town>
2020-05-26 15:00:50 +1000

    Add apollo-client and apollo-utilities to @ts-gql/apollo-client-v2

---
+++ .changeset/ten-dots-sell.md
@@ -0,0 +1,5 @@
+---
+"@ts-gql/apollo-client-v2": minor
+---
+
+Add apollo-client and apollo-utilities
--- packages/apollo-client-v2/src/index.js
+++ packages/apollo-client-v2/src/index.js
@@ -1,2 +1,4 @@
 export * from "@apollo/react-common";
 export * from "@apollo/react-hooks";
+export * from "apollo-client";
+export * from "apollo-utilities";
