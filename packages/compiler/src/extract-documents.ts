1a9803171ee2a412fc5fd50eb5648674b903091c
mitchellhamilton <mitchell@hamil.town>
2022-08-01 15:29:23 +1000

    Scanner experiments

--- packages/compiler/src/extract-documents.ts
+++ packages/compiler/src/extract-documents.ts
@@ -1,6 +1,7 @@
 import type { Node } from "@babel/types";
 import { lazyRequire } from "lazy-require.macro";
-import { CompilerError, FullSourceLocation } from "./types";
+import ts from "typescript";
+import { CompilerError } from "./types";
 
 type BabelVisitors = {
   [Type in Node["type"]]?: (node: Extract<Node, { type: Type }>) => void;
@@ -32,62 +33,95 @@ function babelTraverse(node: Node, visitors: BabelVisitors) {
   }
 }
 
+let tsxScanner: import("typescript").Scanner;
+let tsScanner: import("typescript").Scanner;
+
 export function extractGraphQLDocumentsContentsFromFile(
   filename: string,
   content: string
 ) {
   let errors: CompilerError[] = [];
   let documents: {
-    loc: FullSourceLocation;
+    loc: { start: number; end: number };
     document: string;
   }[] = [];
   if (/gql\s*`/.test(content)) {
     try {
-      let ast = lazyRequire<typeof import("@babel/parser")>().parse(content, {
-        allowImportExportEverywhere: true,
-        allowReturnOutsideFunction: true,
-        allowSuperOutsideMethod: true,
-        sourceType: "module",
-        plugins: [
-          "asyncGenerators",
-          "classProperties",
-          ["decorators", { decoratorsBeforeExport: true }],
-          "doExpressions",
-          "dynamicImport",
-          "typescript",
-          "functionBind",
-          "functionSent",
-          "jsx",
-          "nullishCoalescingOperator",
-          "objectRestSpread",
-          "optionalChaining",
-          "optionalCatchBinding",
-        ],
-        strictMode: false,
-      });
-      babelTraverse(ast as any, {
-        TSAsExpression(node) {
-          // TODO: verify operation name === import
-          if (
-            node.typeAnnotation.type === "TSImportType" &&
-            node.expression.type === "TaggedTemplateExpression"
-          ) {
-            let isGqlTag =
-              node.expression.tag.type === "Identifier" &&
-              node.expression.tag.name === "gql";
-            let hasOnlyWhitespaceAfterFirstInterpolation =
-              node.expression.quasi.quasis
-                .slice(1)
-                .every((x) => x.value.cooked!.trim() === "");
-            if (isGqlTag && hasOnlyWhitespaceAfterFirstInterpolation) {
-              documents.push({
-                loc: node.expression.quasi.loc!,
-                document: node.expression.quasi.quasis[0].value.cooked!,
-              });
-            }
+      const ts = lazyRequire<typeof import("typescript")>();
+
+      let scanner: import("typescript").Scanner;
+      if (filename[filename.length - 1] === "x") {
+        if (tsxScanner === undefined) {
+          tsxScanner = ts.createScanner(
+            ts.ScriptTarget.Latest,
+            false,
+            ts.LanguageVariant.JSX
+          );
+        }
+        scanner = tsxScanner;
+      } else {
+        if (tsScanner === undefined) {
+          tsScanner = ts.createScanner(
+            ts.ScriptTarget.Latest,
+            false,
+            ts.LanguageVariant.Standard
+          );
+        }
+        scanner = tsScanner;
+      }
+      scanner.setText(content);
+      scanner.setTextPos(0);
+      let token = scanner.scan();
+      let lastToken: ts.SyntaxKind = token;
+      let lastWasGql = false;
+      while (token !== ts.SyntaxKind.EndOfFileToken) {
+        if (
+          token === ts.SyntaxKind.Identifier &&
+          lastToken !== ts.SyntaxKind.DotToken
+        ) {
+          if (scanner.getTokenValue() === "gql") {
+            lastWasGql = true;
+            continue;
           }
-        },
-      });
+        } else if (lastWasGql) {
+          if (token === ts.SyntaxKind.NoSubstitutionTemplateLiteral) {
+            const start = scanner.getTokenPos();
+            documents.push({
+              document: scanner.getTokenValue(),
+              loc: {
+                start,
+                end: start + scanner.getTokenText().length,
+              },
+            });
+          }
+        }
+        lastWasGql = false;
+        lastToken = token;
+        token = scanner.scan();
+      }
+
+      // babelTraverse(ast , {
+      //   TSAsExpression(node) {
+      //     if (
+      //       node.typeAnnotation.type === "TSImportType" &&
+      //       node.expression.type === "TaggedTemplateExpression"
+      //     ) {
+      //       let isGqlTag =
+      //         node.expression.tag.type === "Identifier" &&
+      //         node.expression.tag.name === "gql";
+      //       let hasOnlyWhitespaceAfterFirstInterpolation =
+      //         node.expression.quasi.quasis
+      //           .slice(1)
+      //           .every((x) => x.value.cooked!.trim() === "");
+      //       if (isGqlTag && hasOnlyWhitespaceAfterFirstInterpolation) {
+      //         documents.push({
+      //           loc: node.expression.quasi.loc!,
+      //           document: node.expression.quasi.quasis[0].value.cooked!,
+      //         });
+      //       }
+      //     }
+      //   },
+      // });
     } catch (err) {
       if (typeof err.loc?.line === "number") {
         errors.push({
