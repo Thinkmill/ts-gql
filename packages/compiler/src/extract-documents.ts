a5214a275ec625518ab236f004b9dc84780e2504
mitchellhamilton <mitchell@hamil.town>
2022-09-05 12:27:42 +1000

    thing

--- packages/compiler/src/extract-documents.ts
+++ packages/compiler/src/extract-documents.ts
@@ -1,7 +1,5 @@
 import type { Node } from "@babel/types";
 import { lazyRequire } from "lazy-require.macro";
-import ts from "typescript";
-import { CompilerError } from "./types";
 
 type BabelVisitors = {
   [Type in Node["type"]]?: (node: Extract<Node, { type: Type }>) => void;
@@ -33,109 +31,33 @@ function babelTraverse(node: Node, visitors: BabelVisitors) {
   }
 }
 
-let tsxScanner: import("typescript").Scanner;
-let tsScanner: import("typescript").Scanner;
-
 export function extractGraphQLDocumentsContentsFromFile(
   filename: string,
   content: string
 ) {
-  let errors: CompilerError[] = [];
   let documents: {
     loc: { start: number; end: number };
     document: string;
   }[] = [];
   if (/gql\s*`/.test(content)) {
-    try {
-      const ts = lazyRequire<typeof import("typescript")>();
+    const ts = lazyRequire<typeof import("typescript")>();
 
-      let scanner: import("typescript").Scanner;
-      if (filename[filename.length - 1] === "x") {
-        if (tsxScanner === undefined) {
-          tsxScanner = ts.createScanner(
-            ts.ScriptTarget.Latest,
-            false,
-            ts.LanguageVariant.JSX
-          );
-        }
-        scanner = tsxScanner;
-      } else {
-        if (tsScanner === undefined) {
-          tsScanner = ts.createScanner(
-            ts.ScriptTarget.Latest,
-            false,
-            ts.LanguageVariant.Standard
-          );
-        }
-        scanner = tsScanner;
-      }
-      scanner.setText(content);
-      scanner.setTextPos(0);
-      let token = scanner.scan();
-      let lastToken: ts.SyntaxKind = token;
-      let lastWasGql = false;
-      while (token !== ts.SyntaxKind.EndOfFileToken) {
-        if (
-          token === ts.SyntaxKind.Identifier &&
-          lastToken !== ts.SyntaxKind.DotToken
-        ) {
-          if (scanner.getTokenValue() === "gql") {
-            lastWasGql = true;
-            continue;
-          }
-        } else if (lastWasGql) {
-          if (token === ts.SyntaxKind.NoSubstitutionTemplateLiteral) {
-            const start = scanner.getTokenPos();
-            documents.push({
-              document: scanner.getTokenValue(),
-              loc: {
-                start,
-                end: start + scanner.getTokenText().length,
-              },
-            });
-          }
-        }
-        lastWasGql = false;
-        lastToken = token;
-        token = scanner.scan();
-      }
+    const sourceFile = ts.createSourceFile(
+      filename,
+      content,
+      ts.ScriptTarget.Latest,
+      true
+    );
 
-      // babelTraverse(ast , {
-      //   TSAsExpression(node) {
-      //     if (
-      //       node.typeAnnotation.type === "TSImportType" &&
-      //       node.expression.type === "TaggedTemplateExpression"
-      //     ) {
-      //       let isGqlTag =
-      //         node.expression.tag.type === "Identifier" &&
-      //         node.expression.tag.name === "gql";
-      //       let hasOnlyWhitespaceAfterFirstInterpolation =
-      //         node.expression.quasi.quasis
-      //           .slice(1)
-      //           .every((x) => x.value.cooked!.trim() === "");
-      //       if (isGqlTag && hasOnlyWhitespaceAfterFirstInterpolation) {
-      //         documents.push({
-      //           loc: node.expression.quasi.loc!,
-      //           document: node.expression.quasi.quasis[0].value.cooked!,
-      //         });
-      //       }
-      //     }
-      //   },
-      // });
-    } catch (err) {
-      if (typeof err.loc?.line === "number") {
-        errors.push({
-          filename,
-          message: err.message.replace(
-            ` (${err.loc.line}:${err.loc.column})`,
-            ""
-          ),
-          loc: { start: err.loc },
-        });
-      } else {
-        errors.push({ filename, message: err.message });
+    ts.forEachChild(sourceFile, (node) => {
+      if (
+        ts.isAsExpression(node) &&
+        ts.isTaggedTemplateExpression(node.expression) &&
+        node.expression.tag.getText(sourceFile) === "gql" &&
+        ts.isImportTypeNode(node.type)
+      ) {
       }
-    }
+    });
   }
-  return { errors, documents };
+  return { documents };
 }
